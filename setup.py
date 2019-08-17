fc001e23adb485adcf5edbd583fc2775db02a13a
IndigoMad <indigomad@pku.edu.cn>
2019-08-16 17:57:57 -0700

    A test

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='IndigoPy',
+    version='1.0',
+    author='Indigo Li',
+    author_email='indigomad@pku.edu.cn',
+    url='https://github.com/IndigoMad/IndigoPy.git',
+    packages=['IndigoPy','IndigoPy.GUI','IndigoPy.Protein'],
+)
